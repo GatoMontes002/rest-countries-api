cfab88c7af3adcee969341c7852d349ce7445ffe
GatoMontes002 <gatomontes0000@gmail.com>
2024-02-02 09:11:47 -0300

    reacts icons

--- src/componente/encabezado.js
+++ src/componente/encabezado.js
@@ -1,8 +1,10 @@
 import '../estilos/encabezado.css';
+import { FaMoon, FaSun } from "react-icons/fa";
 
 const Encabezado = () => {
     return <header className="encabezado">
-        <h1>Hola Mundo</h1>  
+        <h1>Where in the world?</h1>  
+       <FaMoon/><FaSun/>
         
     </header>;
 };
