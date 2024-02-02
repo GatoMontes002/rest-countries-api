1f37fd38dc3df1de6c9ff33a5deb427acd6375a5
GatoMontes002 <gatomontes0000@gmail.com>
2024-02-02 16:26:23 -0300

    dia y noche

--- src/componente/encabezado.js
+++ src/componente/encabezado.js
@@ -1,12 +1,59 @@
-import '../estilos/encabezado.css';
+import { useState } from "react";
+import "../estilos/encabezado.css";
 import { FaMoon, FaSun } from "react-icons/fa";
 
 const Encabezado = () => {
-    return <header className="encabezado">
-        <h1>Where in the world?</h1>  
-       <FaMoon/><FaSun/>
-        
-    </header>;
+  let [diaOnoche, setDiaOnoche] = useState("light-theme");
+  function cliquear() {
+    console.log(document.body.classList);
+    document.body.classList.toggle("light-theme");
+    document.body.classList.toggle("dark-theme");
+    if (diaOnoche === "light-theme") {
+      setDiaOnoche("dark-theme");
+    } else {
+      setDiaOnoche("light-theme");
+    }
+    let elementos = document.getElementsByClassName("tarjeta");
+    //elementos.classList.toggle("colorTarjetaDia");
+      //  elementos.classList.toggle("colorTarjetaNoche");
+    console.log("***********");
+    console.log(elementos);
+    try {
+      for (let element of elementos) {
+        element.classList.toggle("colorTarjetaDia");
+        element.classList.toggle("colorTarjetaNoche");
+        element.classList.toggle("colorDia");
+        element.classList.toggle("colorNoche");
+      };
+    } catch (Error) {
+      console.log("**errrrrrrr**");
+      console.error(Error);
+    }
+
+    //els.classList.toggle("colorDia");
+    //els.classList.toggle("colorNoche");
+
+    
+  }
+
+  return (
+    <header className="encabezado">
+      <h1 className="izq">Where in the world?</h1>
+      <span className="der">
+        {diaOnoche === "light-theme" ? (
+          <span className="modo-dia-noche" onClick={cliquear}>
+            <FaMoon />
+            &nbsp;<span className="tema">Dark Mode</span>
+          </span>
+        ) : (
+          <span className="modo-dia-noche" onClick={cliquear}>
+            <FaSun />
+            &nbsp;<span className="tema">Light Mode</span>
+          </span>
+        )}
+      </span>
+    </header>
+  );
 };
 
-export {Encabezado};
+export { Encabezado };
--- src/componente/tarjetaPais.js
+++ src/componente/tarjetaPais.js
@@ -1,15 +1,15 @@
 import '../estilos/tarjetaPais.css';
 
 const TarjetaPais = ({nombre,capital,poblacion,bandera}) => {
-    return <article className="tarjeta">
+    return <article className="tarjeta colorTarjetaDia colorDia">
         <div>
             <img src={bandera.png} alt={bandera.alt} className="bandera" />
         </div>
-        <h2 className='fuente800 colorDia'>{nombre}</h2>
+        <h2 className='fuente800'>{nombre}</h2>
 
-        <span className='fuente600 colorDia'>Capital:</span>&nbsp; <span className='fuente300'>{capital}</span>
+        <span className='fuente600'>Capital:</span>&nbsp; <span className='fuente300'>{capital}</span>
         <br/>
-        <span className='fuente600 colorDia'>Population:</span>&nbsp; <span className='fuente300'>{poblacion}</span>
+        <span className='fuente600'>Population:</span>&nbsp; <span className='fuente300'>{poblacion}</span>
         
         
     </article>;
