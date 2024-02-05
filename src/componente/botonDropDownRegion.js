75099da62573e90fc55b931f46ee3c994afbd9b6
GatoMontes002 <gatomontes0000@gmail.com>
2024-02-05 16:15:36 -0300

    link

--- src/componente/botonDropDownRegion.js
+++ src/componente/botonDropDownRegion.js
@@ -12,11 +12,11 @@ function BotonDropDownRegion() {
       </Dropdown.Toggle>
 
       <Dropdown.Menu>
-        <Dropdown.Item href="grilla/africa">Africa</Dropdown.Item>
-        <Dropdown.Item href="grilla/america">America</Dropdown.Item>
-        <Dropdown.Item href="grilla/asia">Asia</Dropdown.Item>
-        <Dropdown.Item href="grilla/europe">Europe</Dropdown.Item>
-        <Dropdown.Item href="grilla/oceania">Oceania</Dropdown.Item>
+        <Dropdown.Item href="/grilla/africa">Africa</Dropdown.Item>
+        <Dropdown.Item href="/grilla/america">America</Dropdown.Item>
+        <Dropdown.Item href="/grilla/asia">Asia</Dropdown.Item>
+        <Dropdown.Item href="/grilla/europe">Europe</Dropdown.Item>
+        <Dropdown.Item href="/grilla/oceania">Oceania</Dropdown.Item>
         <Dropdown.Item href="/">All</Dropdown.Item>
       </Dropdown.Menu>
     </Dropdown>
