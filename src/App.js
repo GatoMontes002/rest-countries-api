a38356c2333de47e87a2b47fa5ac1d2d12854f59
GatoMontes002 <gatomontes0000@gmail.com>
2024-02-06 19:11:16 -0300

    HashRouter

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import { Grilla } from "./componente/grilla";
 import { Encabezado } from "./componente/encabezado";
 import { DetallePais } from "./componente/detallePais";
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import { HashRouter,  Route, Routes } from "react-router-dom";
 
 
 function App() {
@@ -11,14 +11,14 @@ function App() {
       
       <Encabezado />
       <main>
-        <BrowserRouter>
+        <HashRouter>
           <Routes>
           <Route path="/" element={<Grilla/>} />
           <Route path="/grilla/:miregion" element={<Grilla/>} />
           <Route path="/detallepais/:nombrepais" element={<DetallePais  />} />
           <Route path='*' element={<Grilla/>} />
         </Routes>
-        </BrowserRouter>
+        </HashRouter>
       </main>
     </div>
   );
