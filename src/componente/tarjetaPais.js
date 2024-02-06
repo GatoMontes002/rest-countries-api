b3cd74373fd8e55a651c91e302dd8e8f25dfea4b
GatoMontes002 <gatomontes0000@gmail.com>
2024-02-06 13:29:21 -0300

    Alternativa para imagenes

--- src/componente/tarjetaPais.js
+++ src/componente/tarjetaPais.js
@@ -11,9 +11,16 @@ const TarjetaPais = ({ nombre, capital, poblacion, bandera,ccn3,region }) => {
     <article>
       <Link to={ `/detallepais/${ccn3}` }>
         <div className={valor}>
-          <div >
-            <img src={bandera.png} alt={bandera.alt} className="bandera" />
-          </div>
+          { bandera.alt ?
+            <div >
+              <img src={bandera.png} alt={bandera.alt} className="bandera" />
+            </div>
+            :
+            <div >
+              <img src={bandera.png} alt={"Flag of the country: " + nombre} className="bandera" />
+            </div>
+          }
+
           <h2 className="fuente800">{nombre}</h2>
           <span className="fuente600">Population:</span>&nbsp;{" "}
           <span className="fuente300">{poblacion}</span>
